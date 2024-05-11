7b25feef7d9cc214e0d525ef7bc67b41c9065e1d
Ömer Şerefoğlu <omer.salman-98@hotmail.com>
2024-05-11 22:16:04 +0300

    Space Game

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject hazard;
+    public int spawnCount;
+    public float spawnWait;
+    public float StartSpawn;
+    public float waveWait;
+
+    public Text scoreText;
+    public Text gameOverText;
+    public Text restartText;
+    public int score;
+
+    private bool gameOver;
+    private bool restart;
+
+    void Update()
+    {
+        if (restart == true)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+    IEnumerator SpawnValues()
+    {
+        for (int i = 0; i < spawnCount; i++)
+        {
+            yield return new WaitForSeconds(StartSpawn);
+            while (true)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-3, 3), 0, 10);
+                Quaternion spawnRotation = Quaternion.identity;
+
+                Instantiate(hazard, spawnPosition, spawnRotation);
+
+                //Coroutine
+
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if (gameOver == true)
+            {
+                restartText.text = "Press 'R' for Restart";
+                restart = true;
+                break;
+            }
+        }
+    }
+    public void UpdateScore()
+    {
+        score += 10;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over";
+        gameOver = true;
+    }
+    void Start()
+    {
+        gameOverText.text = "";
+        restartText.text = "";
+        gameOver = false;
+        restart = false;
+        StartCoroutine(SpawnValues());
+
+    }
+
+}
